a6444b64cf462ac4c783ccc765eca9c08d397514
Mona Kjeldsrud <mona.kjeldsrud@nav.no>
2024-01-09 15:51:31 +0100

    Opprettet RegelLøsningAdapter

---
+++ src/main/kotlin/no/nav/dagpenger/regel/losningspubliserer/RegelLøsningAdapter.kt
@@ -0,0 +1,58 @@
+package no.nav.dagpenger.regel.losningspubliserer
+
+import mu.KotlinLogging
+import mu.withLoggingContext
+import no.nav.helse.rapids_rivers.JsonMessage
+import no.nav.helse.rapids_rivers.MessageContext
+import no.nav.helse.rapids_rivers.MessageProblems
+import no.nav.helse.rapids_rivers.RapidsConnection
+import no.nav.helse.rapids_rivers.River
+
+private val logger = KotlinLogging.logger { }
+private val sikkerLogg = KotlinLogging.logger("tjenestekall")
+
+class RegelLøsningAdapter(rapidsConnection: RapidsConnection) : River.PacketListener {
+    companion object {
+        val rapidFilter: River.() -> Unit = {
+            validate { it.requireKey("minsteinntektResultat") }
+            validate { it.rejectValue("@prosessertAv", "dp-regel-losningspubliserer") }
+        }
+    }
+
+    init {
+        River(rapidsConnection).apply(rapidFilter).register(this)
+    }
+
+    override fun onError(
+        problems: MessageProblems,
+        context: MessageContext,
+    ) {
+        super.onError(problems, context)
+    }
+
+    override fun onSevere(
+        error: MessageProblems.MessageException,
+        context: MessageContext,
+    ) {
+        super.onSevere(error, context)
+    }
+
+    override fun onPacket(
+        packet: JsonMessage,
+        context: MessageContext,
+    ) {
+        try {
+            val behovId = packet["behovId"].asText()
+            withLoggingContext("behovId" to behovId) {
+                sikkerLogg.info("Mottok løsning for vurdering av minsteinntekt: ${packet.toJson()}")
+                // TODO("Mangler detaljer")
+
+                packet["@prosessertAv"] = "dp-regel-losningspubliserer"
+
+                context.publish(packet.toJson())
+            }
+        } catch (e: Exception) {
+            TODO("Not yet implemented")
+        }
+    }
+}
