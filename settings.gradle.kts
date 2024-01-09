43b4f9baa30989d9fc6447b35e1d143c59ea160c
mkjeldsr <43175419+mkjeldsr@users.noreply.github.com>
2024-01-09 14:51:04 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+rootProject.name = "dp-SERVICENAME"
+dependencyResolutionManagement {
+    repositories {
+        maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+    }
+    versionCatalogs {
+        create("libs") {
+            from("no.nav.dagpenger:dp-version-catalog:20231212.58.e23cad")
+        }
+    }
+}
